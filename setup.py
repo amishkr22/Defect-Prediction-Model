0383fab2e66e4ed4844f6be1dbd46cd005d7e08a
amishkr22 <amishkumar1028@gmail.com>
2025-01-21 18:48:32 +0530

    structure and package updated

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name = 'defect_prediction_model',
+    version='0.0.1',
+    author = 'Amish Nayar',
+    author_email= 'amishkumar1028@gmail.com',
+    install_requires = [],
+    packages=find_packages()
+)
